f309e147f138a364c3c3136f6df908e81a674dcb
Seonghyeon Kim <kshshkim@users.noreply.github.com>
2023-10-31 17:45:50 +0900

    Init: initial commit

---
+++ src/main/java/beforespring/yourfood/YourFoodApplication.java
@@ -0,0 +1,13 @@
+package beforespring.yourfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourFoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YourFoodApplication.class, args);
+    }
+
+}
